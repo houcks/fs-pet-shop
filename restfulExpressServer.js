04009d8c08b381cc777e27c851e26f82ff0966e5
Shaun Houck <shaun@Shauns-MBP.attlocal.net>
2022-03-09 14:29:05 -0700

    Added errorhandling and refactored to use promises

--- restfulExpressServer.js
+++ restfulExpressServer.js
@@ -13,52 +13,81 @@ app
   .route("/pets/:index")
   .get((req, res) => {
     const { index } = req.params;
-    pool.query("SELECT * FROM pets WHERE id=$1", [index], (err, result) => {
-      res.status(201).send(result.rows[0]);
-    });
+    pool.query("SELECT * FROM pets WHERE id=$1", [index])
+    .then((result) => {
+        if(result.rowCount === 0){
+            res.sendStatus(404);
+        }
+        console.log(result.rowCount)
+        res.send(result.rows[0]);
+    })
+    .catch((error) => {
+        res.sendStatus(500);
+    })
   })
   .patch((req, res) => {
     const { index } = req.params;
     const { age, name, kind } = req.body[0];
-    pool.query(
-      `UPDATE pets SET 
-        age = COALESCE($1, age), 
-        name = COALESCE($2, name), 
-        kind = COALESCE($3, kind) 
-        WHERE id=$4 
-        RETURNING *`, [age, name, kind, index], (err,result) =>{
+    pool
+        .query(
+            `UPDATE pets SET 
+            age = COALESCE($1, age), 
+            name = COALESCE($2, name), 
+            kind = COALESCE($3, kind) 
+            WHERE id=$4 
+            RETURNING *`, [age, name, kind, index])
+        .then((result) => {
+            if(result.rowCount === 0){
+                res.sendStatus(404);
+            }
             res.send(result.rows[0])
-        }
-    );   
+        })
+        .catch((err) => {
+            res.sendStatus(500)
+        })
   })
-
   .delete((req, res) => {
     const { index } = req.params;
-    pool.query("DELETE FROM pets WHERE id=$1", [index], (err, result) => {
-      res.status(201).send();
-    });
+    pool
+    .query("DELETE FROM pets WHERE id=$1 RETURNING *", [index])
+    .then((result) => {
+        if(result.rowCount === 0){
+            res.sendStatus(404);
+        }
+        res.send();
+    })
+    .catch((err) => {
+        res.sendStatus(500);
+    })
   });
 
 app
   .route("/pets")
   .get((req, res) => {
-    pool.query("SELECT * FROM pets", (err, result) => {
-      if (err) {
-        res.status(500);
-        res.send();
-      }
-      res.status(201).json(result.rows);
-    });
+    pool
+    .query("SELECT * FROM pets")
+    .then((result) => {
+        res.send(result.rows);
+    })
+    .catch((err) => {
+        res.sendStatus(500);
+    })
   })
   .post((req, res) => {
     const { age, name, kind } = req.body[0];
-    pool.query(
-      "INSERT INTO pets (age, name, kind) VALUES ($1, $2, $3) RETURNING *",
-      [age, name, kind],
-      (err, result) => {
+    pool
+    .query(
+      `INSERT INTO 
+      pets (age, name, kind) 
+      VALUES ($1, $2, $3) 
+      RETURNING *`,
+      [age, name, kind])
+    .then((result) => {
         res.send(result.rows[0]);
-      }
-    );
+    })
+    .catch((err) => {
+        res.sendStatus(500);
+    })
   });
 
 //age kind name
